5502210b6577bf6108718b03f2376fe313dcf3c0
wunaidage <shuaiwang11235@gmail.com>
2019-08-31 01:36:59 -0400

    initial commit

---
+++ webflux-backend/src/main/java/com/wunaidage/sbamwe/webflux/backend/WebFluxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wunaidage.sbamwe.webflux.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxBackendApplication.class, args);
+    }
+
+}
